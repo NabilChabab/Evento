036cfacec9cc402a5798c9f84030e6178b025578
NabilChabab <nabil.chababnabil@gmail.com>
2024-03-02 18:40:44 +0100

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+
+    public function authenticated(Request $request, $user)
+    {
+
+        $roleId = $user->roles()->pluck('role_id')->first();
+
+        if ($roleId == 1) {
+            return redirect('/admin/dashboard');
+        } elseif ($roleId == 2) {
+            return redirect('/home')->with('status' , 'Welcome Back Lets find a Job');
+        } elseif ($roleId == 3) {
+            return redirect('/spectator/home');
+        }else {
+            return redirect($this->redirectTo);
+        }
+    }
+}
